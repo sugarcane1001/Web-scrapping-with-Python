01aa71c24a96370ebd2468fdabe1872d20b91e1b
Mohammed Zian Hassan <139604218+sugarcane1001@users.noreply.github.com>
2023-11-04 18:10:57 -0400

    Create two_table.py

---
+++ two_table.py
@@ -0,0 +1,11 @@
+
+from bs4 import BeautifulSoup
+import requests
+
+two_tables="<h3>Rocket Launch </h3><p><table class='rocket'><tr><td>Flight No</td><td>Launch site</td> <td>Payload mass</td></tr><tr><td>1</td><td>Florida</td><td>300 kg</td></tr><tr><td>2</td><td>Texas</td><td>94 kg</td></tr><tr><td>3</td><td>Florida </td><td>80 kg</td></tr></table></p><p><h3>Pizza Party  </h3><table class='pizza'><tr><td>Pizza Place</td><td>Orders</td> <td>Slices </td></tr><tr><td>Domino's Pizza</td><td>10</td><td>100</td></tr><tr><td>Little Caesars</td><td>12</td><td >144 </td></tr><tr><td>Papa John's </td><td>15 </td><td>165</td></tr>"
+two_tables_bs= BeautifulSoup(two_tables, 'html.parser')
+print(two_tables_bs.prettify())
+
+print(two_tables_bs.find("table"))
+print()
+print(two_tables_bs.find("table",class_='pizza'))
